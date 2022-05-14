993d711e6c4ee0465fc2326fa9cb9c212162a9dc
Evan6684 <evan.hehehe@gmail.com>
2022-05-14 16:35:18 +0800

    icons prototype

--- main.py
+++ main.py
@@ -11,6 +11,14 @@ current_menu = 'EYES'
 menus = {'EYES': {'menu_count': 4}}
 
 
+def drawIcon(icon, center, image):
+    new_img = image.copy()
+    icon = cv2.imread(icon)
+    new_img[center[1]-35:center[1]-35+70,
+            center[0]-35:center[0]-35+70] = icon
+    return new_img
+
+
 def DrawMenu(fingerpos, img, frame):
     init_pos_y = 75
     image = img.copy()
@@ -18,19 +26,34 @@ def DrawMenu(fingerpos, img, frame):
     if current_menu == 'EYES':
         if(frame < 30):
             for i in range(3):
+                imagelink = '/Users/EvanChen/project/facial/icons/icon_' + \
+                    current_menu+'_'+str(i)+'_'+'off.png'
+
                 image = cv2.circle(
                     image, [1150, init_pos_y+i*CIRCLE_DISTANCE-90*i+frame*3*i], CIRCLE_RADIUS, (255, 255, 255), -1)
+                image = drawIcon(
+                    imagelink, [1150, init_pos_y+i*CIRCLE_DISTANCE-90*i+frame*3*i], image)
             return image
         else:
             for i in range(3):
+
                 if FingerTouch([1150, init_pos_y+i*CIRCLE_DISTANCE], fingerpos):
+                    imagelink = '/Users/EvanChen/project/facial/icons/icon_' + \
+                        current_menu+'_'+str(i)+'_'+'off.png'
+
                     image = cv2.circle(
                         image, [1150, init_pos_y+i*CIRCLE_DISTANCE], CIRCLE_RADIUS, (255, 255, 255), -1)
+                    image = drawIcon(
+                        imagelink, [1150, init_pos_y+i*CIRCLE_DISTANCE], image)
                 else:
+                    imagelink = '/Users/EvanChen/project/facial/icons/icon_' + \
+                        current_menu+'_'+str(i)+'_'+'on.png'
+
                     image = cv2.circle(
                         image, [1150, init_pos_y+i*CIRCLE_DISTANCE], CIRCLE_RADIUS, CIRCLE_TOUCH_COLOR, -1)
+                    image = drawIcon(
+                        imagelink, [1150, init_pos_y+i*CIRCLE_DISTANCE], image)
             return image
-
     else:
         return image
 
@@ -55,7 +78,7 @@ while True:
     # Show the image
     image = cv2.flip(image, 1)
     image = DrawMenu([1150, fy], image, fy)
-    if(fy < 30):
+    if(fy < 1000):
         fy += 2
     cv2.imshow("Output", image)
     k = cv2.waitKey(5) & 0xFF
